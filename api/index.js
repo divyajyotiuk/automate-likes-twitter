cd071d37292e6c2e7e5fb83c0230137d081ee0dd
divyajyotiuk <divyajyoti.u@gmail.com>
2020-09-26 15:36:13 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,90 @@
+const env = require('dotenv').config({ path: '../.env' });
+const needle = require('needle');
+const Twit = require('twit');
+/**
+ * AuthorizationHeader
+ * @param method String
+ * @param params Object
+ * @param api String
+ */
+const AuthStringGenerator = require('../service/GenerateAuthHeader');
+
+const TwitterClient = new Twit({
+    consumer_key:         process.env.API_KEY,
+    consumer_secret:      process.env.API_SECRET,
+    access_token:         process.env.ACCESS_TOKEN,
+    access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+});
+
+const token = process.env.BEARER_TOKEN;
+
+const getMentionsApi = 'https://api.twitter.com/2/tweets/search/recent';
+const postLikesApi = 'https://api.twitter.com/1.1/favorites/create.json';
+
+async function getTweetsWithMentions() {
+    const params = {
+        'query': '@divyajyotiuk'
+    } 
+
+    const res = await needle('get', getMentionsApi, params, { headers: {
+        "authorization": `Bearer ${token}`
+    }})
+
+    if(res.body) {
+        let data = res.body && res.body.data;
+        return data.map((i)=>{return i.id});
+    } else {
+        throw new Error ('Unsuccessful request')
+    }
+}
+
+async function postLikesOnMentions(params) {
+    
+    const oAuthHeader = AuthStringGenerator('post', params, postLikesApi);
+
+    const res = await needle('post', postLikesApi, params, { headers: {
+        "authorization": oAuthHeader
+    }})
+
+    if(res.body) {
+        return res.body;
+    } else {
+        throw new Error ('Unsuccessful request')
+    }
+}
+
+module.exports = async (req, res) => {
+    
+}
+
+
+//Uncomment for running on the terminal $ node index.js
+(async () => {
+    try {
+        // Make request
+        const twitterIds = await getTweetsWithMentions();
+        console.log(twitterIds)
+
+        await twitterIds.reduce(async (promise, id) => {
+            await promise; //for the last promise in the chain to resolve
+            const params = {
+                "id": id
+            } 
+            const res = await postLikesOnMentions(params);
+            console.log(res.errors);
+        }, Promise.resolve());
+
+        //very easy using Twit 
+        // const params = {
+        //     id: '1309222192866041857'
+        // }   
+        // await TwitterClient.post('favorites/create', params, function(err, data, response){
+        //     console.log("data :: ",response);
+        // })
+
+    } catch(e) {
+        console.log('catch block ', e);
+        process.exit(-1);
+    }
+    process.exit();
+})();
---
+++ service/GenerateAuthHeader.js
@@ -0,0 +1,48 @@
+const env = require('dotenv').config({ path: '../.env' });
+const cryptoRandomString = require('crypto-random-string');
+const CryptoJS = require("crypto-js");
+const Base64 = require('crypto-js/enc-base64');
+
+const nonce = cryptoRandomString({length: 42, type: 'base64'});
+const signingKey = `${process.env.API_SECRET}&${process.env.ACCESS_TOKEN_SECRET}`;
+const timestamp = Math.round(Date.now()/1000);
+
+function buildParameterString(param){
+    let key = null;
+    let value = null;
+    if(Object.keys(param).length == 1){
+        let keys = Object.keys(param);
+        key = keys[0];
+        value = encodeURIComponent(param[key]);
+    }
+    return `${key}=${value}` + `&` + `oauth_consumer_key=${process.env.API_KEY}` + `&` +
+            `oauth_nonce=${nonce}` + `&` +
+            `oauth_signature_method=HMAC-SHA1` + `&` +
+            `oauth_timestamp=${timestamp}` + `&` +
+            `oauth_token=${process.env.ACCESS_TOKEN}` + `&` +
+            `oauth_version=1.0`;
+}
+
+function buildSignatureBaseString(method, param, api){
+    let methodUppercase = String(method).toUpperCase()
+    ,   encodedApi = encodeURIComponent(api)
+    ,   encodedParamString = encodeURIComponent(buildParameterString(param));
+    ;
+
+    return `${methodUppercase}` + `&` + `${encodedApi}` + `&` + `${encodedParamString}`;
+}
+
+module.exports = function AuthStringGenerator(method, param, api){
+   let signatureBaseString = buildSignatureBaseString(method, param, api)
+   ,   oAuthSignature = Base64.stringify(CryptoJS.HmacSHA1(signatureBaseString, signingKey))
+   ,   encodedAuthSign = encodeURIComponent(oAuthSignature)
+   ;
+
+   return `OAuth oauth_consumer_key="${process.env.API_KEY}", ` + 
+            `oauth_nonce="${nonce}", ` +
+            `oauth_signature="${encodedAuthSign}", ` +
+            `oauth_signature_method="HMAC-SHA1", ` +
+            `oauth_timestamp="${timestamp}", ` + 
+            `oauth_token="${process.env.ACCESS_TOKEN}", ` +
+            `oauth_version="1.0"`;
+}
