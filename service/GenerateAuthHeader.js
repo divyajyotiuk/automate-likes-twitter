0d1cb0d3e2e25badfa7138715255952849764404
divyajyotiuk <divyajyoti.u@gmail.com>
2020-09-26 22:56:15 +0530

    debug v2

--- service/GenerateAuthHeader.js
+++ service/GenerateAuthHeader.js
@@ -3,11 +3,11 @@ const cryptoRandomString = require('crypto-random-string');
 const CryptoJS = require("crypto-js");
 const Base64 = require('crypto-js/enc-base64');
 
-const nonce = cryptoRandomString({length: 42, type: 'base64'});
 const signingKey = `${process.env.API_SECRET}&${process.env.ACCESS_TOKEN_SECRET}`;
-const timestamp = Math.round(Date.now()/1000);
+const nonce = cryptoRandomString({length: 42, type: 'base64'});
 
 function buildParameterString(param){
+    const timestamp = Math.round(Date.now()/1000);
     let key = null;
     let value = null;
     if(Object.keys(param).length == 1){
@@ -15,7 +15,7 @@ function buildParameterString(param){
         key = keys[0];
         value = encodeURIComponent(param[key]);
     }
-    console.log(timestamp);
+    console.log(timestamp, nonce);
     return `${key}=${value}` + `&` + `oauth_consumer_key=${process.env.API_KEY}` + `&` +
             `oauth_nonce=${nonce}` + `&` +
             `oauth_signature_method=HMAC-SHA1` + `&` +
@@ -34,11 +34,12 @@ function buildSignatureBaseString(method, param, api){
 }
 
 module.exports = function AuthStringGenerator(method, param, api){
-   let signatureBaseString = buildSignatureBaseString(method, param, api)
-   ,   oAuthSignature = Base64.stringify(CryptoJS.HmacSHA1(signatureBaseString, signingKey))
-   ,   encodedAuthSign = encodeURIComponent(oAuthSignature)
-   ;
-    console.log(oAuthSignature)
+    const timestamp = Math.round(Date.now()/1000);
+    let signatureBaseString = buildSignatureBaseString(method, param, api)
+    ,   oAuthSignature = Base64.stringify(CryptoJS.HmacSHA1(signatureBaseString, signingKey))
+    ,   encodedAuthSign = encodeURIComponent(oAuthSignature)
+    ;
+    console.log(timestamp, nonce)
    return `OAuth oauth_consumer_key="${process.env.API_KEY}", ` + 
             `oauth_nonce="${nonce}", ` +
             `oauth_signature="${encodedAuthSign}", ` +
